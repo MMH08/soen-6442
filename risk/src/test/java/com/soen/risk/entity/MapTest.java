021c257c24ccab037f4ea4d2ba273806a687263b
Nivetha Ravi <nivetharavi@Nivethas-MacBook-Air.local>
2018-10-17 23:36:48 -0400

    Added validation testcases

--- risk/src/test/java/com/soen/risk/entity/MapTest.java
+++ risk/src/test/java/com/soen/risk/entity/MapTest.java
@@ -30,4 +30,38 @@ public class MapTest {
     @Test
     public void save() {
     }
+    
+    @Test
+  public void isValid(){
+  	//To check continent and country duplication
+      Map map = new Map();
+      map.load("/home/varun/Downloads/001_I72_Ghtroc 720/001_I72_Ghtroc 720.map"); 
+      boolean bvalid = map.isValid();
+      assertFalse(bvalid);
+  }
+  @Test
+  public void isValid2(){
+  	//To check if all adjacent countries are valid countries
+      Map map = new Map();
+      map.load("/home/varun/Downloads/001_I72_Ghtroc 720/001_I72_Ghtroc 720.map"); 
+      boolean bvalid = map.isValid();
+      assertFalse(bvalid);
+  }
+  @Test
+  public void isValid3() {
+  	//To check if no country is isolated
+      Map map = new Map();
+      map.load("/home/varun/Downloads/001_I72_Ghtroc 720/001_I72_Ghtroc 720.map"); 
+      boolean bvalid = map.isValid();
+      assertFalse(bvalid);
+  }
+  @Test
+  public void isValid4(){
+  	//Perfect map
+      Map map = new Map();
+      map.load("/home/varun/Downloads/001_I72_Ghtroc 720/001_I72_Ghtroc 720.map");
+      boolean bvalid = map.isValid();
+      assertTrue(bvalid);																																									
+  }
+
 }
