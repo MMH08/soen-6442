f333b9c5112e4d7dcebe58914719784dcb615231
thahseen03 <m.hinathahseen@gmail.com>
2018-11-27 01:42:00 -0500

    Test case added for AggressiveReinforceStrategy

---
+++ risk/src/test/java/com/soen/risk/entity/player/aggressive/AggressiveReinforceStrategyTest.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.soen.risk.entity.player.aggressive;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.*;
+import org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+import com.soen.risk.entity.Country;
+import com.soen.risk.entity.Map;
+import com.soen.risk.entity.ReinforceStrategy;
+
+
+/**
+ * @author fly
+ *
+ */
+public class AggressiveReinforceStrategyTest {
+	
+	
+	Map map;
+	public AggressiveReinforceStrategy aggReinforceStrategy;
+	
+	@Before
+	public void setUp(){
+			map=new Map();
+			aggReinforceStrategy=new AggressiveReinforceStrategy();
+			map.load("createnew.map"); 
+			
+			
+		}
+	
+	
+	@Test
+	public void executeTest()
+	{
+		
+		ReinforceStrategy.calculateArmyCount(map, map.getCountries());
+		ArrayList<Country> countryList=map.getCountries();
+		ArrayList<Integer> armiesCount=new ArrayList<>();
+		for (Country c:countryList)
+		{
+			armiesCount.add(c.getArmy());
+		}
+		Collections.sort(armiesCount, Collections.reverseOrder());
+		
+		
+		aggReinforceStrategy.execute(map, map.getCountries());
+		countryList=map.getCountries();
+		ArrayList<Integer> armiesCount1=new ArrayList<>();
+		for (Country c:countryList)
+		{
+			armiesCount1.add(c.getArmy());
+		}
+		Collections.sort(armiesCount1, Collections.reverseOrder());
+		
+		
+		assertThat(armiesCount1.get(0) , greaterThan(armiesCount.get(0)));
+		
+			
+		
+		
+		
+	}
+
+}
