83e6a5af4438c93caf7a4a72785ae5552881215a
nivetharavi <nivetharavi@yahoo.co.in>
2018-11-29 04:06:40 -0500

    Replaced while loop by for loop to swap the armies between the strongest and weakest country

--- risk/src/main/java/com/soen/risk/entity/player/benevolent/BenevolentFortifyStrategy.java
+++ risk/src/main/java/com/soen/risk/entity/player/benevolent/BenevolentFortifyStrategy.java
@@ -7,37 +7,43 @@ import com.soen.risk.entity.Map;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Level;
 
 public class BenevolentFortifyStrategy implements FortifyStrategy {
-	
+
+    /**
+     * Start from the weakest country and try to move the army from the strongest country. If the path
+     * does not exist then go for second best strongest country. This goes on till all the countries have
+     * been traversed. Once done,
+     * benevolent player will try to fortify the second weakest country by using the same strategy.
+     * <p>
+     * A special check to avoid swap between same countries in the end of first inner loop.
+     * <p>
+     * Reverse order sort - strongest country --> weakest country
+     *
+     * @param map              Map
+     * @param allowedCountries owned countries by the player
+     * @return if fortification was success.
+     */
     @Override
     public boolean execute(Map map, List<Country> allowedCountries) {
-    	Country startCountry;
-        Country endCountry;
-        int armyCount = 0;
-        int i = 0;
-        Collections.sort(allowedCountries,Collections.reverseOrder(Comparator.comparing(Country::getArmy)));
-    	int weakestCountryindex = allowedCountries.size() - 1;
-    	endCountry = allowedCountries.get(weakestCountryindex);
-    	while(i < weakestCountryindex ) {
-    		startCountry = allowedCountries.get(i);
-    		if (map.pathExists(startCountry, endCountry, allowedCountries)) {
-    			if (startCountry.getArmy()>1 && startCountry.getArmy()%2 == 0) {
-    				armyCount = startCountry.getArmy()/2 ;
-    				startCountry.setArmy (startCountry.getArmy() - armyCount);
-    				endCountry.setArmy (endCountry.getArmy() + armyCount);
-    			}
-    			else if (startCountry.getArmy()>1 && startCountry.getArmy()%2 == 1) {
-    				armyCount = (startCountry.getArmy()-1)/2;
-    				startCountry.setArmy (startCountry.getArmy() - armyCount);
-    				endCountry.setArmy (endCountry.getArmy() + armyCount);
-    			}
-    			return true;
-    		}
-    		else {   	
-    			i++;   	
-    		}
-    	}
-    	return true; 
-     }
+        allowedCountries.sort(Collections.reverseOrder(Comparator.comparing(Country::getArmy)));
+
+        for (int weakestCountryindex = allowedCountries.size() - 1; weakestCountryindex > 0; weakestCountryindex--) {
+            Country endCountry = allowedCountries.get(weakestCountryindex);
+            for (Country startCountry : allowedCountries) {
+                // endCountry is owned by the same player
+                // and there is a path between them then fortify and return true
+                if (!startCountry.getName().equals(endCountry.getName()) && map.pathExists(startCountry, endCountry, allowedCountries)) {
+                    int armyCount = startCountry.getArmy() / 2;
+                    startCountry.setArmy(startCountry.getArmy() - armyCount);
+                    endCountry.setArmy(endCountry.getArmy() + armyCount);
+                    logger.log(Level.INFO, "Fortification between " + startCountry + " and " + endCountry + " count : " + armyCount);
+                    return true;
+                }
+            }
+
+        }
+        return true;
+    }
 }
