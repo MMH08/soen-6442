6385fa3e85290f8a4feb7747707ea20d7e0f2410
varunsinghal <varunsinghal15@gmail.com>
2018-11-29 03:29:59 -0500

    Corrected the usage of won and lost variables, moved attacking country and defending country to field variables, added attack counter to keep track of no. of attacks, this is used to exchange armies, added exchange army call whenever a country is lost by the player

--- risk/src/main/java/com/soen/risk/entity/player/random/RandomAttackStrategy.java
+++ risk/src/main/java/com/soen/risk/entity/player/random/RandomAttackStrategy.java
@@ -4,20 +4,28 @@ import com.soen.risk.entity.AttackStrategy;
 import com.soen.risk.entity.Country;
 import com.soen.risk.entity.Map;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import java.util.logging.Level;
 
 public class RandomAttackStrategy implements AttackStrategy {
+    private int attackCounter;
     private List<Country> won;
     private HashMap<Country, Country> lost;
     private boolean isComplete;
+    private Country attackingCountry;
+    private Country defendingCountry;
 
     public RandomAttackStrategy() {
         this.won = new ArrayList<>();
         this.lost = new HashMap<>();
         this.isComplete = true;
+        this.attackCounter = 0;
     }
 
-    private List<Country> attackedCountry(Map map, Country attackingCountry, List<Country> countries) {
+    private List<Country> attackedCountry(Map map, List<Country> countries) {
         List<Country> ll = map.getNeighbouringCountry(attackingCountry);
         List<Country> attackedCountry = new ArrayList<Country>();
         for (Country c : ll) {
@@ -35,121 +43,70 @@ public class RandomAttackStrategy implements AttackStrategy {
         return attackedCountry;
     }
 
-    private int normalizeAttackDiceCount(Country attackingCountry) {
+    private int normalizeAttackDiceCount() {
         if (attackingCountry.getArmy() >= 3) return 3;
         if (attackingCountry.getArmy() == 2) return 2;
         if (attackingCountry.getArmy() == 1) return 1;
         return 0;
     }
 
-    private int normalizeDefendingDiceCount(Country defendingCountry, Country attackingCountry) {
+    private int normalizeDefendingDiceCount() {
         if (defendingCountry.getArmy() >= 2) return 2;
         if (defendingCountry.getArmy() == 1) return 1;
         if (attackingCountry.getArmy() == 1) return 1;
         return 0;
     }
 
-    private ArrayList<Boolean> playGame(Country attackingCountry, Country attackedCountry) {
-        ArrayList<Boolean> win = new ArrayList<>();
+    private ArrayList<Boolean> playGame() {
 
-        int attackingDiceCount = normalizeAttackDiceCount(attackingCountry);
-        int defendingDiceCount = normalizeDefendingDiceCount(attackedCountry, attackingCountry);
-
-        if (defendingDiceCount <= 0 || attackingDiceCount <= 0) {
-            return win;
-        }
-
-        int dicI1[] = new int[attackingDiceCount];
-        int dicI2[] = new int[defendingDiceCount];
-
-
-        for (int i = 0; i < dicI1.length; i++) {
-            dicI1[i] = 1 + (int) (9.0 * Math.random());
-        }
-
-        for (int i = 0; i < dicI2.length; i++) {
-            dicI2[i] = 1 + (int) (9.0 * Math.random());
-        }
-        Arrays.sort(dicI1);
-        Arrays.sort(dicI2);
-
-        if (dicI1.length == 1) {
-            if (dicI1[0] >= dicI2[0]) {
-                win.add(true);
-            } else {
-                win.add(false);
-            }
-        } else if (dicI1.length == 2) {
-            if (dicI2.length == 1) {
-                if (dicI1[1] >= dicI2[0]) {
-                    win.add(true);
-                } else {
-                    win.add(false);
-                }
-            } else if (dicI2.length == 2) {
-                if (dicI1[1] >= dicI2[1]) {
-                    win.add(true);
-                } else {
-                    win.add(false);
-                }
-                if (dicI1[0] >= dicI2[0]) {
-                    win.add(true);
-                } else {
-                    win.add(false);
-                }
-            }
-        } else if (dicI1.length == 3) {
-            if (dicI2.length == 1) {
-                if (dicI1[2] >= dicI2[0]) {
-                    win.add(true);
-                } else {
-                    win.add(false);
-                }
-            } else if (dicI2.length == 2) {
-                if (dicI1[2] >= dicI2[1]) {
-                    win.add(true);
-                } else {
-                    win.add(false);
-                }
-                if (dicI1[1] >= dicI2[0]) {
-                    win.add(true);
-                } else {
-                    win.add(false);
-                }
-            }
-        }
-        return win;
+        int attackingDiceCount = normalizeAttackDiceCount();
+        int defendingDiceCount = normalizeDefendingDiceCount();
+        attackCounter = attackCounter + defendingDiceCount; //revisit this code
+        return AttackStrategy.simulateDiceRoll(attackingDiceCount, defendingDiceCount);
 
     }
 
+    /**
+     * Random player - attack strategy
+     * Changes: added attackCounter, corrected the lost and won assignment,
+     *
+     * @param map Map
+     * @param countries list of owned countries
+     */
     @Override
     public void execute(Map map, List<Country> countries) {
-        Country attackingCountry = countries.get(countries.size() / 2);
-        List<Country> ListAttackedCountry = this.attackedCountry(map, attackingCountry, countries);
-        Country attackedCountry = ListAttackedCountry.get(ListAttackedCountry.size() / 2);
+        attackingCountry = countries.get(countries.size() / 2);
+        List<Country> ListAttackedCountry = this.attackedCountry(map, countries);
+        defendingCountry = ListAttackedCountry.get(ListAttackedCountry.size() / 2);
+        logger.log(Level.INFO, "Attacking country : " + attackingCountry + ", defending : " + defendingCountry);
+
         int flag = 0;
         int j = 0;
         Random rand = new Random();
         int randomNumber = 1 + rand.nextInt(3);
+
         while (flag != 1 || j < randomNumber) {
-            ArrayList<Boolean> b = new ArrayList<Boolean>();
-            b = this.playGame(attackingCountry, attackedCountry);
-            for (int i = 0; i < b.size(); i++) {
-                if (b.get(i) == true) {
+            ArrayList<Boolean> wins = this.playGame();
+            for (boolean win: wins) {
+                if (win) {
+                    logger.log(Level.INFO, "Attacker won 1 army.");
                     attackingCountry.setArmy(attackingCountry.getArmy() + 1);
-                    attackedCountry.setArmy(attackedCountry.getArmy() - 1);
+                    defendingCountry.setArmy(defendingCountry.getArmy() - 1);
                 } else {
+                    logger.log(Level.INFO, "Defender won 1 army.");
                     attackingCountry.setArmy(attackingCountry.getArmy() - 1);
-                    attackedCountry.setArmy(attackedCountry.getArmy() + 1);
+                    defendingCountry.setArmy(defendingCountry.getArmy() + 1);
                 }
             }
-            if (attackedCountry.getArmy() == 0) {
-                won.add(attackingCountry);
-                lost.put(attackedCountry, attackingCountry);
+            if (defendingCountry.getArmy() == 0) {
+                logger.log(Level.INFO, "Defending country lost all the army");
+                AttackStrategy.exchangeArmy(attackingCountry, defendingCountry, attackCounter);
+                won.add(defendingCountry);
                 flag = 1;
             } else if (attackingCountry.getArmy() == 0) {
-                won.add(attackedCountry);
-                lost.put(attackingCountry, attackedCountry);
+                logger.log(Level.INFO, "Attacking country lost all the army");
+                AttackStrategy.exchangeArmy(defendingCountry, attackingCountry, attackCounter);
+                lost.put(attackingCountry, defendingCountry);
                 flag = 1;
             }
             j++;
