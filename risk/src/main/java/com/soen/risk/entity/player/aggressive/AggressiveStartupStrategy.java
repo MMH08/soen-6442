72551ccf98b03dfb939491963c451573c010dd3f
manmeet99 <manmeet.singh2308@gmail.com>
2018-11-29 05:36:46 -0500

    Added startup phase execution for aggressive player

--- risk/src/main/java/com/soen/risk/entity/player/aggressive/AggressiveStartupStrategy.java
+++ risk/src/main/java/com/soen/risk/entity/player/aggressive/AggressiveStartupStrategy.java
@@ -4,8 +4,17 @@ import com.soen.risk.entity.Country;
 import com.soen.risk.entity.StartupStrategy;
 
 public class AggressiveStartupStrategy implements StartupStrategy {
+    /**
+     * @param country      in which army has to be assigned
+     * @param armyCapacity total army count available
+     * @return consumed army count
+     */
     @Override
     public int execute(Country country, int armyCapacity) {
-        return 0;
+        if (armyCapacity > 0) {
+            country.addArmy(1);
+            return 1;
+        }
+        return armyCapacity;
     }
 }
